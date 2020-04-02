eb4573f7f8c1a38bd2c1a820015ed267e2542a2b
Dillon <dillon@bridgements.com>
2020-04-02 09:24:57 +0200

    Removed custom css and js

--- corona-virus-covid19-banner.php
+++ corona-virus-covid19-banner.php
@@ -3,38 +3,29 @@
  * Plugin Name: corona-virus-covid19-banner
  * Plugin URI: https://www.bridgement.com
  * Description: Display South African COVID-19 banner
- * Version: 0.2.1
+ * Version: 0.2.2
  * Author: Bridgement
  * License: GPL2
  *
  * @package corona-virus-covid19-banner
- * @version 0.2.1
+ * @version 0.2.2
  * @author Bridgement <support@bridgement.com>
  */
 
 add_action( 'wp_enqueue_scripts', 'covid_banner' );
 function covid_banner() {
-        // Enqueue the style
+
 		wp_register_style('covid-banner-style',  plugin_dir_url( __FILE__ ) .'corona-virus-covid19-banner.css', '', VERSION);
         wp_enqueue_style('covid-banner-style');
 
         $script_params = array(
-			// // script specific parameters
-			// 'covid_banner_text' => get_option('covid_banner_text'),
-			// 'pro_version_enabled' => get_option('pro_version_enabled'),
 			'in_array' => in_array(get_the_ID(), explode(",", get_option('disabled_pages_array'))),
-			// // debug specific parameters
 			'debug_mode' => get_option('debug_mode'),
             'id' => get_the_ID(),
             'img_src' => plugin_dir_url( __FILE__ ) .'img/coat.png',
-			// 'disabled_pages_array' => explode(",", get_option('disabled_pages_array')),
 			'covid_banner_color' => get_option('covid_banner_color'),
 			'covid_banner_text_color' => get_option('covid_banner_text_color'),
 			'covid_banner_link_color' => get_option('covid_banner_link_color'),
-			// 'covid_banner_text' => get_option('covid_banner_text'),
-			// 'covid_banner_custom_css' => get_option('covid_banner_custom_css'),
-			// 'site_custom_css' => get_option('site_custom_css'),
-			// 'site_custom_js' => get_option('site_custom_js')
 		);
 
 
@@ -42,7 +33,6 @@ function covid_banner() {
 		wp_enqueue_style ( 'font-script');
 
 
-        // Enqueue the script
         wp_register_script('covid-banner-script', plugin_dir_url( __FILE__ ) . 'corona-virus-covid19-banner.js', array( 'jquery' ), VERSION);
         wp_localize_script('covid-banner-script', 'scriptParams', $script_params);
         wp_enqueue_script('covid-banner-script');
@@ -101,12 +91,11 @@ function covid_banner_settings_page() {
 
 		<p>Use Hex color values for the color fields.</p>
 
-		<!-- Settings Form -->
 		<form method="post" action="options.php">
 			<?php settings_fields( 'covid-banner-settings-group' ); ?>
 			<?php do_settings_sections( 'covid-banner-settings-group' ); ?>
 			<table class="form-table">
-				<!-- Background Color -->
+
 				<tr valign="top">
 					<th scope="row">Covid Banner Background Color<br><span style="font-weight:400;">(Leaving this blank sets the color to the default value)</span></th>
 					<td style="vertical-align:top;">
@@ -116,7 +105,7 @@ function covid_banner_settings_page() {
 										value="<?php echo ((get_option('covid_banner_color') == '') ? '#024985' : esc_attr( get_option('covid_banner_color') )); ?>">
 					</td>
 				</tr>
-				<!-- Text Color -->
+
 				<tr valign="top">
 					<th scope="row">Covid Banner Text Color<br><span style="font-weight:400;">(Leaving this blank sets the color to the default value)</span></th>
 					<td style="vertical-align:top;">
@@ -135,7 +124,7 @@ function covid_banner_settings_page() {
 										value="<?php echo ((get_option('covid_banner_header_color') == '') ? '#030303' : esc_attr( get_option('covid_banner_header_color') )); ?>">
 					</td>
 				</tr>
-				<!-- Link Color-->
+
 				<tr valign="top">
 					<th scope="row">Covid Banner Link Color<br><span style="font-weight:400;">(Leaving this blank sets the color to the default value)</span></th>
 					<td style="vertical-align:top;">
@@ -147,113 +136,10 @@ function covid_banner_settings_page() {
 				</tr>
 			</table>
 
-			<!-- Save Changes Button -->
+
 			<?php submit_button(); ?>
 		</form>
 	</div>
-
-	<!-- Script to apply styles to Preview Banner -->
-	<script type="text/javascript">
-		var style_background_color = document.createElement('style');
-		var style_link_color = document.createElement('style');
-		var style_text_color = document.createElement('style');
-
-		// Banner Text
-
-		// Background Color
-		style_background_color.type = 'text/css';
-		style_background_color.id = 'preview_banner_background_color'
-		style_background_color.appendChild(document.createTextNode('.covid-banner{background:' + (document.getElementById('covid_banner_color').value || '#024985') + '}'));
-		document.getElementsByTagName('head')[0].appendChild(style_background_color);
-
-		document.getElementById('covid_banner_color').onchange=function(e){
-			document.getElementById('covid_banner_color_show').value = e.target.value || '#024985';
-			var child = document.getElementById('preview_banner_background_color');
-			if (child){child.innerText = "";child.id='';}
-
-			var style_dynamic = document.createElement('style');
-			style_dynamic.type = 'text/css';
-			style_dynamic.id = 'preview_banner_background_color';
-			style_dynamic.appendChild(
-				document.createTextNode(
-					'.covid-banner{background:' + (document.getElementById('covid_banner_color').value || '#024985') + '}'
-				)
-			);
-			document.getElementsByTagName('head')[0].appendChild(style_dynamic);
-		};
-		document.getElementById('covid_banner_color_show').onchange=function(e){
-			document.getElementById('covid_banner_color').value = e.target.value;
-			document.getElementById('covid_banner_color').dispatchEvent(new Event('change'));
-		};
-
-		// Text Color
-		style_text_color.type = 'text/css';
-		style_text_color.id = 'preview_banner_text_color'
-		style_text_color.appendChild(document.createTextNode('.covid-banner .covid-banner-text{color:' + (document.getElementById('covid_banner_text_color').value || '#ffffff') + '}'));
-		document.getElementsByTagName('head')[0].appendChild(style_text_color);
-
-		document.getElementById('covid_banner_text_color').onchange=function(e){
-			document.getElementById('covid_banner_text_color_show').value = e.target.value || '#ffffff';
-			var child = document.getElementById('preview_banner_text_color');
-			if (child){child.innerText = "";child.id='';}
-
-			var style_dynamic = document.createElement('style');
-			style_dynamic.type = 'text/css';
-			style_dynamic.id = 'preview_banner_text_color';
-			style_dynamic.appendChild(
-				document.createTextNode(
-					'.covid-banner .covid-banner-text{color:' + (document.getElementById('covid_banner_text_color').value || '#ffffff') + '}'
-				)
-			);
-			document.getElementsByTagName('head')[0].appendChild(style_dynamic);
-		};
-		document.getElementById('covid_banner_text_color_show').onchange=function(e){
-			document.getElementById('covid_banner_text_color').value = e.target.value;
-			document.getElementById('covid_banner_text_color').dispatchEvent(new Event('change'));
-		};
-
-		// Link Color
-		style_link_color.type = 'text/css';
-		style_link_color.id = 'preview_banner_link_color'
-		style_link_color.appendChild(document.createTextNode('.covid-banner .covid-banner-text a{color:' + (document.getElementById('covid_banner_link_color').value || '#f16521') + '}'));
-		document.getElementsByTagName('head')[0].appendChild(style_link_color);
-
-		document.getElementById('covid_banner_link_color').onchange=function(e){
-			document.getElementById('covid_banner_link_color_show').value = e.target.value || '#065fd4';
-			var child = document.getElementById('preview_banner_link_color');
-			if (child){child.innerText = "";child.id='';}
-
-			var style_dynamic = document.createElement('style');
-			style_dynamic.type = 'text/css';
-			style_dynamic.id = 'preview_banner_link_color';
-			style_dynamic.appendChild(
-				document.createTextNode(
-					'.covid-banner .covid-banner-text a{color:' + (document.getElementById('covid_banner_link_color').value || '#065fd4') + '}'
-				)
-			);
-			document.getElementsByTagName('head')[0].appendChild(style_dynamic);
-		};
-		document.getElementById('covid_banner_link_color_show').onchange=function(e){
-			document.getElementById('covid_banner_link_color').value = e.target.value;
-			document.getElementById('covid_banner_link_color').dispatchEvent(new Event('change'));
-		};
-
-		// Disabled Pages
-		document.getElementById('covid_banner_pro_disabled_pages').onclick=function(e){
-			let disabledPagesArray = [];
-			Array.from(document.getElementById('covid_banner_pro_disabled_pages').getElementsByTagName('input')).forEach(function(e) {
-				if (e.checked) {
-					disabledPagesArray.push(e.value);
-				}
-			});
-			document.getElementById('disabled_pages_array').value = disabledPagesArray;
-		};
-
-		// remove banner text newlines on submit
-		document.getElementById('submit').onclick=function(e){
-			document.getElementById('covid_banner_text').value = document.getElementById('covid_banner_text').value.replace(/\n/g, "");
-		};
-	</script>
 	<?php
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Plugin URI: https://www.bridgement.com/
 Author: Bridgement
 Requires at least: 3.0.1
 Tested up to: 5.3.2
-Stable tag: 0.2.1
+Stable tag: 0.2.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
